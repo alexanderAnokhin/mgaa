b358f30ec0024506f3bef396308a77dff2ccf1bc
Alexander Anokhin <a_anok01@uni-muenster.de>
2016-01-20 18:22:07 +0100

    Utils

---
+++ Assets/Completed/Scripts/Utils/Utils.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Completed;
+
+public static class Utils {
+    /*public static List<MovingObject> GetEnemies () {
+        
+    }*/
+    
+    public static GameObject GetPlayerGameObject () {
+        return GameObject.FindGameObjectWithTag ("Player");
+    }
+
+    public static Player GetPlayer () {
+        return GetPlayerGameObject ().GetComponent <Player> ();
+    }
+
+    public static Transform GetPlayerTransform () {
+        return GetPlayerGameObject ().GetComponent <Transform> ();
+    }
+}
