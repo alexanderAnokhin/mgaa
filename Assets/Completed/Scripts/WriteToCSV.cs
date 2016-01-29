3fe08b3601bc12945338c78ebdb843b2a86a96be
MRenka <marcel.renka@gmail.com>
2016-01-29 15:21:41 +0100

    Changed WriteToCSV to work.

--- Assets/Completed/Scripts/WriteToCSV.cs
+++ Assets/Completed/Scripts/WriteToCSV.cs
@@ -36,9 +36,9 @@ namespace Completed
         public void AppendTitles()
         {
             StringBuilder titleText = new StringBuilder("DateTime,Day,");
-            titleText.Append("Min Wall Tiles,Max Wall Titles,Wall Tiles,Wall Tiles Target Value,Exploration");
-            titleText.Append("Min Food Tiles,Max Food Tiles,Food Tiles,Food Tiles Target Value,FoodFitness");
-            titleText.Append("Enemies,Enemy coverage,");
+            titleText.Append("Min Wall Tiles,Max Wall Titles,Wall Tiles,Wall Tiles Target Value,Exploration,");
+            titleText.Append("Min Food Tiles,Max Food Tiles,Food Tiles,Food Tiles Target Value,Food Fitness,");
+            titleText.Append("Enemies,Enemy coverage");
             sw.WriteLine(titleText.ToString());
             sw.Flush();
         }
@@ -46,19 +46,17 @@ namespace Completed
         public void AppendSolution(double[,] solution)
         {
             string dateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-            StringBuilder solutionText = new StringBuilder();
-
             for (int i = 0; i < 10; i++)
             {
-                solutionText.Append(dateTime + "," + level + ",");
+                StringBuilder solutionText = new StringBuilder(dateTime + "," + level + ",");
                 for (int j = 2; j < 13; j++)
                 {
-                    solutionText.Append(solution[i, j] + ",");
+                   solutionText.Append(solution[i, j] + ","); 
                 }
-                solutionText.Append(solution[i, 13]);                
+                solutionText.Append(solution[i, 13]);
+                sw.WriteLine(solutionText.ToString());
+                sw.Flush();
             }
-            sw.WriteLine(solutionText.ToString());
-            sw.Flush();
         }
 
         public void Stop()
