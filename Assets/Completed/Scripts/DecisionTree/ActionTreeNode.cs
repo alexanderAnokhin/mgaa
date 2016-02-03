2dd9c63356e5ecf7ee7937d57d1bb5a4c3898f95
Tac713 <TTobollik@gmail.com>
2016-02-03 14:31:24 +0100

    added decision tree controller and changes in utils

---
+++ Assets/Completed/Scripts/DecisionTree/ActionTreeNode.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ActionTreeNode : DecisionTreeNode {
+
+    //Does the action itself
+    abstract public void doAction(int sightRng, out int xDir, out int yDir);
+}
---
+++ Assets/Completed/Scripts/DecisionTree/DecisionTreeNode.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class DecisionTreeNode{
+
+    //Recursively walkes through the tree
+    abstract public ActionTreeNode makeDecision(int sightRng);
+}
